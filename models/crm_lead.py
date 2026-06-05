968f15b44e331ad8679c64f244ea2ad6d8e54438
Abin Joshua Hermon <abinjoshua3@gmail.com>
2026-06-05 17:49:09 +0530

    [UPDT]05/06/2026

--- models/crm_lead.py
+++ models/crm_lead.py
@@ -7,31 +7,86 @@ class CrmLead(models.Model):
 
     @api.model
     def get_tiles_data(self):
-        # print('r')
         company_id = self.env.company
         leads = self.search([('company_id', '=', company_id.id),
                              ('user_id', '=', self.env.user.id)])
-        # print(leads)
         my_leads = leads.filtered(lambda r: r.type == 'lead')
         my_opportunity = leads.filtered(lambda r: r.type == 'opportunity')
         currency = company_id.currency_id.symbol
         expected_revenue = sum(my_opportunity.mapped('expected_revenue'))
-        won_lead = leads.search([('stage_id', '=', 4),])
-        # print(won_lead)
+        won_lead = leads.search([('stage_id', '=', 4), ])
         revenue = sum(won_lead.mapped('expected_revenue'))
-        # print(revenue)
         win_ratio = int((len(won_lead) / len(leads)) * 100)
-        # print('win_ratio',win_ratio)
-        lost_leads = leads.search([('active', '=', False),
-                                   ('probability','=',0),
-                                   ('user_id', '=', self.env.user.id)])
-        lost_leads_name = lost_leads.mapped('name')
+
+
+        lost_leads = self.read_group(
+            [('active', '=', False),
+             ('probability', '=', 0),
+             ('user_id', '=', self.env.user.id)],
+            ['name'],
+            ['name']
+        )
+
+        lost_leads_labels = []
+        lost_leads_values = []
+
+        for lost_lead in lost_leads:
+            lost_leads_labels.append(lost_lead['name'])
+            lost_leads_values.append(lost_lead['name_count'])
+
+        activity_data = self.env['mail.activity'].read_group(
+            [('user_id', '=', self.env.user.id)],
+            ['activity_type_id'],
+            ['activity_type_id']
+        )
+
+        activity_labels = []
+        activity_values = []
+
+        for activity in activity_data:
+            activity_labels.append(activity['activity_type_id'][1])
+            activity_values.append(activity['activity_type_id_count'])
+
+        campaign_leads = self.read_group(
+            [('user_id', '=', self.env.user.id)],
+            ['campaign_id'],
+            ['campaign_id']
+        )
+
+        campaign_leads_labels = []
+        campaign_leads_values = []
+
+        for campaign_lead in campaign_leads:
+            campaign_leads_labels.append(campaign_lead['campaign_id'][1])
+            campaign_leads_values.append(campaign_lead['campaign_id_count'])
+
+        leads_medium = self.read_group(
+            [('user_id', '=', self.env.user.id)],
+            ['medium_id'],
+            ['medium_id']
+        )
+
+        medium_labels = []
+        medium_values = []
+
+        for medium in leads_medium:
+            medium_labels.append(medium['medium_id'][1])
+            medium_values.append(medium['medium_id_count'])
+
+        print('mediums labels',medium_labels)
+        print('mediums values',medium_values)
+
         return {
             'total_leads': len(my_leads),
             'total_opportunity': len(my_opportunity),
             'expected_revenue': expected_revenue,
             'currency': currency,
             'revenue': revenue,
-            'win_ratio': str(win_ratio)+"%",
-            'lost_leads': lost_leads_name,
+            'win_ratio': str(win_ratio) + "%",
+            'activity_labels': activity_labels,
+            'activity_values': activity_values,
+            'lost_leads_labels': lost_leads_labels,
+            'lost_leads_values': lost_leads_values,
+            'campaign_leads_labels': campaign_leads_labels,
+            'campaign_leads_values': campaign_leads_values
         }
--- static/src/js/dashboard.js
+++ static/src/js/dashboard.js
@@ -17,18 +17,43 @@ class CrmDashboard extends Component {
         let result = await this.orm.call("crm.lead", "get_tiles_data", [], {});
         document.getElementById('my_lead').innerHTML = `<span>${result.total_leads}</span>`;
         document.getElementById('win_ratio').innerHTML = `<span>${result.win_ratio}</span>`;
-        document.getElementById('lost_lead').innerHTML = `<span>${result.lost_lead}</span>`;
         document.getElementById('my_opportunity').innerHTML = `<span>${result.total_opportunity}</span>`;
         document.getElementById('expected_revenue').innerHTML = `<span>${result.currency}${result.expected_revenue}</span>`;
         document.getElementById('total_revenue').innerHTML = `<span>${result.currency}${result.revenue}</span>`;
         await loadJS(["/web/static/lib/Chart/Chart.js"])
 
-        var chart = new Chart("chart_example", {
+        var chart = new Chart("activity_pie_chart", {
             type: "pie",
             data: {
-                labels: [10, 20, 30, 40, 50],
+                labels: result.activity_labels,
                 datasets: [{
-                    data: [10, 20, 30, 40, 50],
+                    data: result.activity_values,
+                    pointBackgroundColor: "black",
+                }]
+            },
+            option: {}
+        });
+
+        var chart = new Chart("lost_lead_graph", {
+            type: "bar",
+            data: {
+                labels: result.lost_leads_labels,
+                datasets: [{
+                    label:'Lost Lead Graph',
+                    data: result.lost_leads_values,
+                    pointBackgroundColor: "black",
+                }]
+            },
+            option: {}
+        });
+
+        var chart = new Chart("campaign_lead_graph", {
+            type: "line",
+            data: {
+                labels: result.campaign_leads_labels,
+                datasets: [{
+                    label:'Campaign Lead Graph',
+                    data: result.campaign_leads_values,
                     pointBackgroundColor: "black",
                 }]
             },
